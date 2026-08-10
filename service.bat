bbc1d1f597fae812054d211297e517596356acc1
qkryt <armmxz0@gmail.com>
2026-08-10 09:29:12 +0300

    Update service.bat

--- service.bat
+++ service.bat
@@ -1,5 +1,5 @@
 @echo off
-set "LOCAL_VERSION=1.10.1"
+set "LOCAL_VERSION=1.4.2"
 
 :: External commands
 if "%~1"=="status_zapret" (
@@ -372,8 +372,8 @@ cls
 
 :: Set current version and URLs
 set "GITHUB_VERSION_URL=https://raw.githubusercontent.com/Flowseal/zapret-discord-youtube/main/.service/version.txt"
-set "GITHUB_RELEASE_URL=https://github.com/Flowseal/zapret-discord-youtube/releases/tag/"
-set "GITHUB_DOWNLOAD_URL=https://github.com/Flowseal/zapret-discord-youtube/releases/latest"
+set "GITHUB_RELEASE_URL=https://github.com/qkryt/zapret-geometry-dash/releases/tag/"
+set "GITHUB_DOWNLOAD_URL=https://github.com/qkryt/zapret-geometry-dash/releases/latest"
 
 :: Get the latest version from GitHub
 for /f "delims=" %%A in ('powershell -NoProfile -Command "(Invoke-WebRequest -Uri \"%GITHUB_VERSION_URL%\" -Headers @{\"Cache-Control\"=\"no-cache\"} -UseBasicParsing -TimeoutSec 5).Content.Trim()" 2^>nul') do set "GITHUB_VERSION=%%A"
@@ -461,7 +461,7 @@ echo:
 tasklist /FI "IMAGENAME eq AdguardSvc.exe" | find /I "AdguardSvc.exe" > nul
 if !errorlevel!==0 (
     call :PrintRed "[X] Adguard process found. Adguard may cause problems with Discord"
-    call :PrintRed "https://github.com/Flowseal/zapret-discord-youtube/issues/417"
+    call :PrintRed "https://github.com/qkryt/zapret-geometry-dash/issues/417"
 ) else (
     call :PrintGreen "Adguard check passed"
 )
@@ -471,7 +471,7 @@ echo:
 sc query | findstr /I "Killer" > nul
 if !errorlevel!==0 (
     call :PrintRed "[X] Killer services found. Killer conflicts with zapret"
-    call :PrintRed "https://github.com/Flowseal/zapret-discord-youtube/issues/2512#issuecomment-2821119513"
+    call :PrintRed "https://github.com/qkryt/zapret-geometry-dash/issues/2512#issuecomment-2821119513"
 ) else (
     call :PrintGreen "Killer check passed"
 )
@@ -1171,7 +1171,7 @@ exit /b
 where %1 >nul 2>&1
 if %errorLevel% neq 0 (
     echo [ERROR] %1 not found in PATH
-    echo Fix your PATH variable with instructions here https://github.com/Flowseal/zapret-discord-youtube/issues/7490
+    echo Fix your PATH variable with instructions here https://github.com/qkryt/zapret-geometry-dash/issues/7490
     pause
     exit /b 1
 )
